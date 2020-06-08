029d1ff2246197399c6b3e2e9f35817015507929
Sandip Pal <sandippal@yahoo.com>
2020-06-08 00:30:30 -0700

    Added labels and parametric dynamic graphs

--- python/plotInfectedTrends.py
+++ python/plotInfectedTrends.py
@@ -1,26 +1,48 @@
+# Plotting trends on infections 
+# Author : Sandip Pal & 
+# Start Date: 06/06/2020
+# How to use this script: TBD
+
+
+
 import plotly.figure_factory as ff
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-key_name = 'Alameda, California, US'
+key_name = 'San Luis Obispo, California, US'
+
 
 df_sample = pd.read_csv('../data/csejhudump/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv', index_col="Combined_Key")
 
-lastDays=10
+lastDays=20 # Trend for the last how many days
 
-alameda_row = df_sample.loc[key_name]
-print(alameda_row.index[len(alameda_row)-lastDays:])
+oneRow = df_sample.loc[key_name]
+#Debug prints
+print(oneRow.index[len(oneRow)-lastDays:])
+print(oneRow.values[len(oneRow)-lastDays:])
 
-print(alameda_row.values[len(alameda_row)-lastDays:])
 
 
+plt.figure(figsize=(32, 8))
 
-plt.figure(figsize=(28, 8))
+plt.plot(oneRow.index[len(oneRow)-lastDays:], oneRow.values[len(oneRow)-lastDays:], 'bo-')
+plt.xlabel(f'Last {lastDays}')
+plt.ylabel(key_name)
 
-plt.subplot(131)
-plt.plot(alameda_row.index[len(alameda_row)-lastDays:], alameda_row.values[len(alameda_row)-lastDays:])
+# zip joins x and y coordinates in pairs
+for x,y in zip(oneRow.index[len(oneRow)-lastDays:],oneRow.values[len(oneRow)-lastDays:]):
+
+    label = "{:.2f}".format(y)
+
+    plt.annotate(label, # this is the text
+                 (x,y), # this is the point to label
+                 textcoords="offset points", # how to position the text
+                 xytext=(0,10), # distance from text to points (x,y)
+                 ha='center') # horizontal alignment can be left, right or center
+
+
 
 plt.show()
 
