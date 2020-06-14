7721f1fe95a09be51ff5ede837da4502c338f40d
Sandip Pal <sandippal@yahoo.com>
2020-06-14 08:24:11 -0700

    1. Add World death trend per country bar and cumilative 2. Modified world country Infected plot

---
+++ python/plotDeathTrendsWorld.py
@@ -0,0 +1,72 @@
+# Plotting trends on infections 
+# Author : Sandip Pal  
+# Start Date: 06/06/2020
+# How to use this script: TBD
+
+
+
+import plotly.figure_factory as ff
+
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt1
+import numpy as np
+import pandas as pd
+
+key_name = 'India'
+
+
+df_sample = pd.read_csv('../data/csejhudump/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv', index_col="Country/Region")
+
+lastDays=20 # Trend for the last how many days
+
+#Debug prints
+#print(df_sample)
+
+oneRow = df_sample.loc[key_name]
+#Debug prints
+print(oneRow.index[len(oneRow)-lastDays:])
+print(oneRow.values[len(oneRow)-lastDays:])
+
+res = [oneRow.values[i + 1] - oneRow.values[i] for i in range(len(oneRow.values)-1)] 
+
+plt.figure(figsize=(24, 8))
+plt.subplot(131)
+plt.plot(oneRow.index[len(oneRow)-lastDays:], oneRow.values[len(oneRow)-lastDays:], 'bo-')
+plt.xlabel(f'Last {lastDays} days')
+plt.ylabel(key_name)
+
+# zip joins x and y coordinates in pairs
+for x,y in zip(oneRow.index[len(oneRow)-lastDays:],oneRow.values[len(oneRow)-lastDays:]):
+
+    label = "{:.2f}".format(y)
+
+    plt.annotate(label, # this is the text
+                 (x,y), # this is the point to label
+                 textcoords="offset points", # how to position the text
+                 xytext=(0,10), # distance from text to points (x,y)
+                 ha='center') # horizontal alignment can be left, right or center
+
+
+
+
+#plt.figure(figsize=(24, 8))
+plt.subplot(132)
+plt.bar(oneRow.index[len(oneRow)-lastDays:], res[len(res)-lastDays:])
+plt.xlabel(f'Last {lastDays} days')
+plt.ylabel(key_name)
+
+# zip joins x and y coordinates in pairs
+for x,y in zip(oneRow.index[len(oneRow)-lastDays:],  res[len(res)-lastDays:]):
+
+    label = "{:.2f}".format(y)
+
+    plt.annotate(label, # this is the text
+                 (x,y), # this is the point to label
+                 textcoords="offset points", # how to position the text
+                 xytext=(0,10), # distance from text to points (x,y)
+                 ha='center') # horizontal alignment can be left, right or center
+
+
+
+plt.show()
+
--- python/plotInfectedTrendsUS.py
+++ python/plotInfectedTrendsUS.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-key_name = 'Shasta, California, US'
+key_name = 'Alameda, California, US'
 
 df_sample = pd.read_csv('../data/csejhudump/COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv', index_col="Combined_Key")
 
@@ -29,7 +29,7 @@ print(oneRow.values[len(oneRow)-lastDays:])
 
 plt.figure(figsize=(32, 8))
 
-plt.plot(oneRow.index[len(oneRow)-lastDays:], oneRow.values[len(oneRow)-lastDays:], 'bo-')
+plt.bar(oneRow.index[len(oneRow)-lastDays:], oneRow.values[len(oneRow)-lastDays:])
 plt.xlabel(f'Last {lastDays}')
 plt.ylabel(key_name)
 
